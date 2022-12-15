0f82b2f5a915ec216e1682285b7710bfe3948fe3
Byeong-Kyu Ahn <byeongkyu@gmail.com>
2022-12-15 21:12:56 +0900

    Move gripper and get status

--- robotiq_gripper_controller/src/main_node.cpp
+++ robotiq_gripper_controller/src/main_node.cpp
@@ -1,13 +1,20 @@
 #include "rclcpp/rclcpp.hpp"
 #include <modbus/modbus.h>
+#include "robotiq_gripper_interfaces/msg/gripper_status.hpp"
+#include "robotiq_gripper_interfaces/msg/gripper_command.hpp"
 
 using namespace std::chrono_literals;
+using std::placeholders::_1;
 
 class RobotiqGripperController: public rclcpp::Node
 {
     public:
         RobotiqGripperController() : Node("robotiq_gripper_controller")
         {
+            req_gripper_pos_ = 0.0;
+            req_gripper_speed_= 0.0;
+            req_gripper_force_ = 0.0;
+
             this->declare_parameter<std::string>("port_name", "/dev/ttyUSB0");
             this->declare_parameter<int>("baudrate", 115200);
             this->declare_parameter<double>("rate", 20.0);
@@ -19,7 +26,16 @@ class RobotiqGripperController: public rclcpp::Node
 
             modbus_ = modbus_new_rtu(port_name.c_str(), baudrate, 'N', 8, 1);
             assert(modbus_ != NULL);
-            // assert(modbus_connect(modbus_) != -1);
+            modbus_set_slave(modbus_, 9);
+            if(modbus_connect(modbus_) == -1)
+            {
+                modbus_free(modbus_);
+                assert(false);
+            }
+
+            sub_gripper_command_ = this->create_subscription<robotiq_gripper_interfaces::msg::GripperCommand>(
+                "gripper_command", 10, std::bind(&RobotiqGripperController::gripper_command_callback, this, _1));
+            pub_gripper_status_ = this->create_publisher<robotiq_gripper_interfaces::msg::GripperStatus>("gripper_status", 10);
 
             auto period = std::chrono::duration<double>(1.0 / this->get_parameter("rate").as_double());
             timer_ = this->create_wall_timer(period, std::bind(&RobotiqGripperController::timer_callback, this));
@@ -33,17 +49,68 @@ class RobotiqGripperController: public rclcpp::Node
         void timer_callback()
         {
             // Send command to Gripper
+            uint16_t send_registers[3] = {0, 0, 0};
 
+            send_registers[0] = 0x0900;  // gACT and gGTO is always on
+            send_registers[1] = req_gripper_pos_ & 0xFF;
+            send_registers[2] = (req_gripper_speed_ << 8) + (req_gripper_force_);
+
+            modbus_write_registers(modbus_, 0x3E8, 3, send_registers);
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
             // Receive status from Gripper
             uint16_t recv_registers[3] = {0, };
-            modbus_read_registers(0x7D0, 3, recv_registers);
+            modbus_read_registers(modbus_, 0x7D0, 3, recv_registers);
+
+            auto msg = robotiq_gripper_interfaces::msg::GripperStatus();
+            msg.g_act = (uint8_t)(recv_registers[0] >> 8) & 0x01;
+            msg.g_gto = (uint8_t)(recv_registers[0] >> 11) & 0x01;
+            msg.g_sta = (uint8_t)(recv_registers[0] >> 12) & 0x03;
+            msg.g_obj = (uint8_t)(recv_registers[0] >> 14) & 0x03;
+            msg.g_flt = (uint8_t)(recv_registers[1] >> 8) & 0x0F;
+            msg.g_pr = (uint8_t)(recv_registers[1]) & 0xFF;
+            msg.g_po = (uint8_t)(recv_registers[2] >> 8) & 0xFF;
+            msg.g_cu = (uint8_t)(recv_registers[2]) & 0xFF;
+
+            pub_gripper_status_->publish(msg);
+        }
+
+        void gripper_command_callback(const robotiq_gripper_interfaces::msg::GripperCommand::SharedPtr msg)
+        {
+            if(msg->position > 1.0 && msg->position < 0.0)
+            {
+                RCLCPP_WARN(this->get_logger(), "Gripper positon is in [0...1]...");
+                return;
+            }
+
+            if(msg->speed > 1.0 && msg->speed < 0.0)
+            {
+                RCLCPP_WARN(this->get_logger(), "Gripper speed is in [0...1]...");
+                return;
+            }
+
+            if(msg->force > 1.0 && msg->force < 0.0)
+            {
+                RCLCPP_WARN(this->get_logger(), "Gripper force is in [0...1]...");
+                return;
+            }
+
+            req_gripper_pos_ = (uint8_t)(msg->position * 255.0);
+            req_gripper_speed_ = (uint8_t)(msg->speed * 255.0);
+            req_gripper_force_ = (uint8_t)(msg->force * 255.0);
         }
 
     private:
         rclcpp::TimerBase::SharedPtr timer_;
         modbus_t *modbus_;
 
+        uint8_t req_gripper_pos_;
+        uint8_t req_gripper_speed_;
+        uint8_t req_gripper_force_;
+
+        rclcpp::Publisher<robotiq_gripper_interfaces::msg::GripperStatus>::SharedPtr pub_gripper_status_;
+        rclcpp::Subscription<robotiq_gripper_interfaces::msg::GripperCommand>::SharedPtr sub_gripper_command_;
 };
 
 int main(int argc, char * argv[])
